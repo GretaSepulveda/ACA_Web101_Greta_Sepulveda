b2165e0d4b79877e6f8bfcc49031a56888c09985
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2019-04-16 21:47:56 -0500

    checkpoint 2 updates

---
+++ myPortfolio/src/scripts/tickTacToe.js
@@ -0,0 +1,25 @@
+// choose player: X & 0
+ // select location on board
+  // append x or o to location
+   // change player 
+
+var player
+var prevPlayer
+
+function selectX (){
+    player1 = 'X'
+    console.log('player:', player)
+}
+function select0 ()
+
+function setValue (element) {
+    console.log('player', player)
+} else if (!player) {
+    alert('select a player')
+    } else if (prevPlayer===player) {
+    element.innerText = player
+    prevPlayer=player
+    alert('you are a liar and a thief. pick new player!')
+} else {
+    element.innerText = player
+    prevPlayer= player

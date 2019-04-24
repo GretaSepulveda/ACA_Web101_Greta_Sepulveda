2698ad4e25d8b3d90af45911ef0aa40548c56bdb
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2019-04-24 16:59:56 -0500

    tictactoe

--- myPortfolio/src/scripts/tickTacToe.js
+++ myPortfolio/src/scripts/tickTacToe.js
@@ -1,25 +1,33 @@
-// choose player: X & 0
- // select location on board
-  // append x or o to location
-   // change player 
+    // choose player: X & O
+    // select location on board
+    // append x or o to location
+    // change player 
 
-var player
-var prevPlayer
+    console.log('hello')
+    
+    var player
+    var prevPlayer
 
-function selectX (){
-    player1 = 'X'
+    function selectX () {
+    player = 'X'
     console.log('player:', player)
-}
-function select0 ()
+    }
+    function selectO () {
+        player = "O"
+    }
+    function setValue(el) {
+    //console.log('player', player)
+    if (!player) {
+        alert('select a player') 
+    } else if (prevPlayer === player) {
 
-function setValue (element) {
-    console.log('player', player)
-} else if (!player) {
-    alert('select a player')
-    } else if (prevPlayer===player) {
-    element.innerText = player
-    prevPlayer=player
-    alert('you are a liar and a thief. pick new player!')
-} else {
-    element.innerText = player
-    prevPlayer= player
+    } else {
+        el.innerText = player
+        prevPlayer = player
+    }
+}
+
+
+     
+
+  
